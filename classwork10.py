a106a8ec7092359a2bf2eba8faa2096804bc2030
yusrazaid <79062047+yusrazaid@users.noreply.github.com>
2022-10-07 16:37:58 +0100

    Add files via upload
    Class 10

---
+++ classwork10.py
@@ -0,0 +1,71 @@
+num=int(input("Enter a number"))
+while(num>0):
+    if(num%5==0 and num%3==0):
+        print("Your number is divisble by five and three")
+        break
+    else:
+        print("Your number is not divisible by three or five")
+        break
+i=1
+while i<=10:
+    if i==5:
+        break
+    i+=1
+    print(i)
+
+n=int(input("Enter a number")
+if (n>0):
+      o=n//10
+      t=n//100
+      h=n//1000
+ 
+elif(o>t and h):
+    print(o,"Is the greatest digit in the number")
+elif(t>h and o):
+    print(t,"Is the greatest digit in the number")
+elif(h>t and o):
+    print(h,"Is the greatest digit in the number")
+else:
+    print("Your number is not valid")
+#2    
+n=int(input("Enter a number")
+if (n>0):
+      
+      o=n//10
+      t=n//100
+      h=n//1000
+ 
+elif(o<t and h):
+    print(o,"Is the smallest digit in the number")
+elif(t<h and o):
+    print(t,"Is the smallest digit in the number")
+elif(h<t and o):
+    print(h,"Is the smallest digit in the number")
+else:
+    print("Your number is not valid")
+#3
+u=int(input("Enter a three digit number"))
+c=u//10
+z=u//100
+x=u//1000
+print(c)
+print(z)
+print(x)
+
+
+
+
+            
+    
+
+    
+    
+    
+
+    
+
+    
+    
+    
+
+
